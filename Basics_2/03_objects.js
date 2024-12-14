8c53fc72fbdeee0e9f5dfc1f63752e1b23826f88
divanshu <garg20divanshu@gmail.com>
2024-12-15 00:19:26 +0530

    updated object file

--- Basics_2/03_objects.js
+++ Basics_2/03_objects.js
@@ -1,10 +1,12 @@
 // singleton
 // object.create topic in further videos
 
-
+const mySymbol = Symbol("key1");
 // object literal
 const  JsUser ={
     name: "Divanshu",
+    "full name": "Divanshu Garg",
+    [mySymbol]: "UserKey1",
     age: 21,
     location: "Noida",
     email: "divanshu20@gmail.com",
@@ -12,5 +14,23 @@ const  JsUser ={
     lastLoginDays: ["Monday", "Thursday"]
 }
 
-console.log(JsUser.age);
-console.log(JsUser[email]);
+// console.log(JsUser.age);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySymbol]); 
+
+// JsUser.email = "esehikoi@gmail.com";
+// Object.freeze(JsUser);
+// JsUser.email = "chatgptkabhai@gmail.com";
+// console.log(JsUser);
+
+JsUser.greetings = function(){
+    console.log("Hello JS user");
+}
+
+JsUser.greetingstwo = function(){
+    console.log(`Hello JS user ${this.name}`);
+}
+
+
+console.log(JsUser.greetingstwo());
